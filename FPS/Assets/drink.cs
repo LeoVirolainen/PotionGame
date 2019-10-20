55a159fc87349602552e4f5df22e976f314ebf00
LeoVirolainen <virolainen.leo@gmail.com>
2019-10-20 13:33:54 +0300

    drink animation

---
+++ FPS/Assets/drink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drink : MonoBehaviour {
+
+    public Animator anim;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            anim.SetBool("drink", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0)) {
+            anim.SetBool("drink", false);
+        }
+    }
+}
