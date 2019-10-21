2dfded161428f23ca1ba3e1f13aa199b7f3a3ee4
Emilin4t0r <virolainen.emil@gmail.com>
2019-10-21 17:03:21 +0300

    Unity version update and potion holder code

---
+++ FPS/Assets/Scripts/potionHolderController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potionHolderController : MonoBehaviour {
+    public GameObject redPotion;
+    public GameObject bluePotion;
+    public GameObject greenPotion;
+
+    public Color sleeveColor = Color.yellow;
+
+    private void Start() {
+        //GameObject.Find("red_fluid").GetComponent<Renderer>().material.color = sleeveColor;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            if (redPotion.activeSelf == false) {
+                redPotion.SetActive(true);
+            } else if (redPotion.activeSelf == true && Input.GetKeyDown(KeyCode.Alpha2) && Input.GetKeyDown(KeyCode.Alpha3)){
+                redPotion.SetActive(false);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            if (bluePotion.activeSelf == false) {
+                bluePotion.SetActive(true);
+            }
+            else if (bluePotion.activeSelf == true && Input.GetKeyDown(KeyCode.Alpha1) && Input.GetKeyDown(KeyCode.Alpha3)) {
+                bluePotion.SetActive(false);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            if (greenPotion.activeSelf == false) {
+                greenPotion.SetActive(true);
+            }
+            else if (greenPotion.activeSelf == true && Input.GetKeyDown(KeyCode.Alpha1) && Input.GetKeyDown(KeyCode.Alpha2)) {
+                greenPotion.SetActive(false);
+            }
+        }
+        
+    }
+}
